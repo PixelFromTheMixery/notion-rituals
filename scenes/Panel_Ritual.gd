ca7a34f4ddf332cb596c73a549e48df6f52fc2d8
ThePixelMixery <83445650+ThePixelMixery@users.noreply.github.com>
2024-12-03 01:15:33 +1100

    Ritual Sequence Summary

--- scenes/Panel_Ritual.gd
+++ scenes/Panel_Ritual.gd
@@ -1,26 +1,28 @@
 extends Panel
 
-@onready var no_ritual_warning = $Label_Start
-@onready var sequence_screen = $VBox_List
-@onready var sequence_list = $VBox_List/Scroll_Sequence/VBox_Sequence
-@onready var ritual_screen = $VBox_Ritual
+@onready var no_ritual_warning: Label = $Label_Start
+@onready var sequence_screen: VBoxContainer = $VBox_List
+@onready var sequence_info: Label = $VBox_List/Label_SequenceInfo
+@onready var ritual_screen: VBoxContainer = $VBox_Ritual
+
 
 func _ready():
-	signals.connect("ritual_selected", generate_sequence)
+	signals.connect("ritual_selected", show_sequence)
 
-func generate_sequence():
+func show_sequence():
 	ritual_screen.hide()
 	no_ritual_warning.hide()
 	sequence_screen.show()
+
+	var sequence_count = str(global.data[global.ritual].keys().size())+" steps will take "
+	
+	var step_time_total = 0
 	for step in global.data[global.ritual]:
-		var step_panel = Label.new()
-		step_panel.text = step
-		sequence_list.add_child(step_panel)
+		step_time_total += global.data[global.ritual][step]['time']
+	var sequence_time = str(step_time_total) + " minutes"
+
+	sequence_info.text = sequence_count + sequence_time
 
 func _on_button_start_pressed():
-	print(sequence_list.get_child_count())
-	while sequence_list.get_child_count() != 0:
-		sequence_list.get_child(0).queue_free()
-		print(sequence_list.get_child_count())
 	sequence_screen.hide()
 	ritual_screen.show()
---
+++ scenes/prefabs/step_info.gd
@@ -0,0 +1,7 @@
+extends HBoxContainer
+
+class_name step_info
+
+@onready var time = $Label_Time
+@onready var title = $Label_Step
+@onready var step_type = $Label_Type
